81312a4c99d0652fb5ce985f866c00ca135410fa
garc1a04 <120516207+garc1a04@users.noreply.github.com>
2024-10-31 16:41:01 -0300

    Novidades: Jogo com fisica e imagens

--- FlappyBird.py
+++ FlappyBird.py
@@ -2,23 +2,55 @@ import pygame
 
 """
 Author: Guilherme Garcia Monteiro
-
 """
 
-
-
 #Criando a tela
-largura = 300
-altura = 400
+largura = 500
+altura = 650
 tela = pygame.display.set_mode((largura,altura))
+frames = pygame.time.Clock()
+
+#imagens
+passaro = pygame.image.load("Imagens\\birdComanemia.png")
+fundo = pygame.image.load("Imagens\\bg.png")
+chao = pygame.image.load("Imagens\\ground.png")
+
+#Variaveis
+chaoX = 0
+andarX = 0
+andarY = altura//2
+velocidade = 0
+clicou = False
+run = True
 
 #Repeticao infinita para o jogo
-while True:
+while run:
+    frames.tick(60)
+    
+    tela.fill((0,0,0))
+    tela.blit(fundo, (0, 0))
+    tela.blit(passaro, (andarX, andarY))
+    tela.blit(chao, (chaoX, 600))
+    
     for event in pygame.event.get():
-        if event.type == quit:
+        if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-
-
-    pygame.display.update()
+            
+        if pygame.mouse.get_pressed()[0] == 1 and clicou == False:
+            clicou = True
+            andarY-=10
+            velocidade = -10
+        if pygame.mouse.get_pressed()[0] == 0:
+            clicou = False
     
+    #Chão andando
+    chaoX-=4
+    if abs(chaoX) > 35:
+        chaoX = 0
+    
+    #Gravidade
+    velocidade += 0.5
+    if(andarY < 565):
+        andarY += velocidade
+    pygame.display.update()
